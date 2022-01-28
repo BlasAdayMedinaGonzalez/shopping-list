43f38c78d222e093efaac1d4f6f0eeab155191b2
BlasAdayMedinaGonzalez <usr2702@salesianos-lacuesta.net>
2022-01-28 09:48:11 +0000

    Feature: Added product input and button

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputGroup}>
+        <TextInput
+          style={styles.productInput}
+          placeholder="Add a new product to shopping list"
+        />
+        <Button style={styles.productButton} title="Add" />
+      </View>
+      <View></View>
     </View>
   );
 }
 
+/** Estilo, crear para cada conjunto de elementos */
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ccc",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flex: 1
+  },
+  inputGroup: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderColor: "black",
+    borderWidth: 1,
+    width: "100%",
+    padding: 20,
+  },
+  productInput: {
+    width: "75%",
+    borderBottomColor: "teal",
+    borderBottomWidth: 1,
+    height: 40,
+  },
+  productButton: {
+    width: "25%",
   },
 });

ed39d8a3fa0410fd0c7bf69a886916711b8fc72f
BlasAdayMedinaGonzalez <usr2702@salesianos-lacuesta.net>
2022-01-28 10:44:35 +0000

    Feature: Added an onPress handler and managed a product list in state

--- App.js
+++ App.js
@@ -4,21 +4,43 @@ import { Button, StyleSheet, TextInput, View } from "react-native";
 
 export default function App() {
   const [productName, setproductName] = useState("");
+  const [productList, setproductList] = useState([]);
   
   const productNameHandler = (productText) => {
     setproductName(productText);
     console.log(productText);
   }
+
+  const addproductHandler = () => {
+    if (productName !== "") {
+      setproductList((currentProductList) => [...currentProductList, productName]);
+      setproductName("");
+    }
+  }
+
+  // const addproductHandler = () => {
+  //   // setproductList([...productList, productName]) forma mal
+  //   setproductList((currentProductList) => {
+  //     const newState = [...currentProductList, productName]
+  //     console.log("El productList -> ", newState);
+  //     return newState;
+  //   });
+  //   setproductName("");
+  //   // Forma bien sin mostrar
+  //   // setproductList((currentProductList) => [...currentProductList, productName]);
+  // }
+  
   return (
     <View style={styles.container}>
       <View style={styles.inputGroup}>
         <TextInput
           style={styles.productInput}
           placeholder="Add a new product to shopping list"
+          value={productName}
           onChangeText={productNameHandler}
           //(userText) => setproductName(userText)
         />
-        <Button style={styles.productButton} title="Add" />
+        <Button style={styles.productButton} title="Add" onPress={addproductHandler} />
       </View>
       <View></View>
     </View>
